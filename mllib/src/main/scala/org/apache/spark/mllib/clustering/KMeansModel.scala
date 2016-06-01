5802a9cd0d466b1b19f55a1e2462ad6c838fd03d
Abraham <mouendless@gmail.com>
2016-06-01 13:35:42 +0800

    Update KMeansModel.scala

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/KMeansModel.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/KMeansModel.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// test
 package org.apache.spark.mllib.clustering
 
 import scala.collection.JavaConverters._
